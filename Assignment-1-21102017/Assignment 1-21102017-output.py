bbd9eac66e56050332e1b8b1fbe40f5e00eb2086
21102017 Rishant Thakur <99980838+rishantthakur@users.noreply.github.com>
2022-04-25 09:31:26 +0530

    Add files via upload

---
+++ Assignment-1-21102017/Assignment 1-21102017-output.py
@@ -0,0 +1,24 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:/Users/acer/Downloads/Assignment 1-21102017-input.py =======
+
+enter 1st number: 20
+enter 2nd number: 30
+enter 3rd number: 100
+avg of numbers = 50.0
+
+Enter Gross Income: 50000
+Enter No. Of Dependent: 3
+Taxable Income: 31000
+Tax: 6200.0
+
+Student:  ['SID', 'Name', 'Gender', 'Department Name', 'CGPA']
+student:  [21102017, 'Rishant Thakur', 'M', 'Civil', 10]
+
+Marks Before Sorting:  [70, 95, 88, 62, 83]
+Marks After Sorting:  [62, 70, 83, 88, 95]
+
+Color ['Red', 'Green', 'White', 'black', 'Pink', 'Yellow']
+(a) Color ['Red', 'Green', 'White', 'Pink', 'Yellow']
+(b) Color ['Red', 'Green', 'White', 'Purple', 'Yellow']
