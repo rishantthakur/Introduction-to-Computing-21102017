941b21e24ca5bb3a56a57c9be40e1af1521264a4
21102017 Rishant Thakur <99980838+rishantthakur@users.noreply.github.com>
2022-06-18 22:57:57 +0530

    Add files via upload

---
+++ Assignment_5_21102017/Assignment_5_21102017_output.py
@@ -0,0 +1,248 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: D:/Assignment_5_21102017_input.py ==================
+
+Enter the string to be printed in reverse order :
+Hey Iam Rishant
+
+The reversed order of string entered is :
+tnahsiR maI yeH
+
+
+Enter the initial number of range :
+1
+
+Enter the last(ending) number of range :
+15
+
+Enter the divisor for entered range :
+4
+
+Numbers divisile in the range are :
+1 . 4
+
+2 . 8
+
+3 . 12
+
+
+Enter the lenght of 1st side of triangle :
+3
+
+Enter the lenght of 2nd side of triangle :
+4
+
+Enter the lenght of 3rd side of triangle :
+5
+
+Area of triangle whose sides are entered is : 6.0
+
+
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+Enter number of rows to be printed :
+4
+A
+BC
+DEF
+GHIJ
+Please, Enter the Lowest Range Value: 1
+Please, Enter the Upper Range Value: 10
+The Prime Numbers in the range are: 
+2
+
+3
+
+5
+
+7
+
+The numbers are divisible by only 7 not 11
+7
+14
+21
+28
+35
+42
+49
+56
+63
+70
+84
+91
+98
+105
+112
+119
+126
+133
+140
+147
+161
+168
+175
+182
+189
+196
+203
+210
+217
+224
+238
+245
+252
+259
+266
+273
+280
+287
+294
+301
+315
+322
+329
+336
+343
+350
+357
+364
+371
+378
+392
+399
+406
+413
+420
+427
+434
+441
+448
+455
+469
+476
+483
+490
+497
+The numbers are only divisible by only 11 not 7
+11
+22
+33
+44
+55
+66
+88
+99
+110
+121
+132
+143
+165
+176
+187
+198
+209
+220
+242
+253
+264
+275
+286
+297
+319
+330
+341
+352
+363
+374
+396
+407
+418
+429
+440
+451
+473
+484
+495
+The numbers are divisible by both 7 and 11
+77
+
+154
+
+231
+
+308
+
+385
+
+462
+
+
+Enter the 1st number to be checked :
+4
+Enter the 2nd number to be checked :
+44
+Enter the 3rd number to be checked :
+5
+Enter the 4th number to be checked :
+76
+Enter the 5th number to be checked :
+5
+Enter the 6th number to be checked :
+45
+Enter the 7th number to be checked :
+65
+Enter the 8th number to be checked :
+36
+Enter the 9th number to be checked :
+5
+Enter the 10th number to be checked :
+3
+
+4 is a positive as well as even number.
+
+44 is a positive as well as even number.
+
+5 is a positive as well as odd number
+
+76 is a positive as well as even number.
+
+5 is a positive as well as odd number
+
+45 is a positive as well as odd number
+
+65 is a positive as well as odd number
+
+36 is a positive as well as even number.
+
+5 is a positive as well as odd number
+
+3 is a positive as well as odd number
+
+The following input numbers are odd :
+ , 5 , 5 , 45 , 65 , 5 , 3
+
+The following input numbers are positive :
+ , 4 , 44 , 5 , 76 , 5 , 45 , 65 , 36 , 5 , 3
+
+The following input numbers are negative :
+
+
+The following input numbers are even :
+ , 4 , 44 , 76 , 36
+
+Enter the number to be checked for number of appearences :
+5
+The number appeared 3 times.
+
+Enter a string:Hey Iam Rishant Thakur
+{'Hey': 1, 'Iam': 1, 'Rishant': 1, 'Thakur': 1}
